a5cb08dfc0128954b307940892089b98991ecef2
Aniket Raj <aniketraj.go@gmail.com>
2019-12-12 23:53:05 +0530

    class

--- C++/hr2.cpp
+++ C++/hr2.cpp
@@ -1,3 +1,14 @@
+/*You have to create a class, named Student, representing the student's details, as mentioned above,
+and store the data of a student. Create setter and getter functions for each element; that is, 
+the class should at least have following functions:
+get_age, set_age
+get_first_name, set_first_name
+get_last_name, set_last_name
+get_standard, set_standard
+Also, you have to create another method to_string() which returns the string consisting of the above elements,
+separated by a comma(,). You can refer to stringstream for this.
+*/
+
 #include <iostream>
 #include <sstream>
 using namespace std;
