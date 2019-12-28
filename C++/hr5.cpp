6ad343a4b7be2b2ac3dbe8cfb75b303df786103b
Aniket Raj <aniketraj.go@gmail.com>
2019-12-28 22:36:59 +0530

    pointer

--- C++/hr5.cpp
+++ C++/hr5.cpp
@@ -1,7 +1,31 @@
+/*You have to complete the function void update(int *a,int *b), which reads two integers as argument, and sets  with the sum of them, and  with the absolute difference of them.
+
+Input Format
+
+Input will contain two integers,  and , separated by a newline.
+
+Output Format
+
+You have to print the updated value of  and , on two different lines.
+
+
+P.S.: Input/ouput will be automatically handled. You only have to complete the void update(int *a,int *b) function.
+
+Sample Input
+
+4
+5
+Sample Output
+
+9
+1
+*/
 #include <stdio.h>
+#include<math.h>
 
 void update(int *a,int *b) {
-    // Complete this function    
+    *a=(*a)+(*b);
+    *b=abs((*a)-2*(*b)); 
 }
 
 int main() {
